bfd6324aa7395de4f53c8adb774234922857e7d0
dearpollapat <70456916+dearpollapat@users.noreply.github.com>
2022-05-17 08:30:52 +0700

    Add files via upload

---
+++ 16 TextToSpeech/start.js
@@ -0,0 +1,21 @@
+const message = new SpeechSynthesisUtterance();
+
+function onVoicesChanged() {
+  const voices = speechSynthesis.getVoices();
+  const thVoice = voices.find((voice) => voice.lang === "th-TH");
+  message.voice = thVoice;
+}
+
+function onClick(event) {
+  message.text = event.target.getAttribute("alt");
+  speechSynthesis.speak(message);
+}
+
+function run() {
+  speechSynthesis.addEventListener("voiceschanged", onVoicesChanged);
+
+  const imgElems = Array.from(document.querySelectorAll("img"));
+  imgElems.forEach((imgelm) => imgelm.addEventListener("click", onClick));
+}
+
+run();
